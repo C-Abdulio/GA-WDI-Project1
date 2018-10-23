ea9051e64db46283aae73dc1e277b3fa794662e7
CarlosAznable <carabdu@gmail.com>
2018-10-23 19:54:15 -0400

    scrolling animation established. time to seperate!

--- GA-WDI-project1/js/main.js
+++ GA-WDI-project1/js/main.js
@@ -1,24 +1,41 @@
 const player = document.querySelector("#player-Rev");//RevRad, your robot racer
 const board = document.querySelector("#board");//the board element and the name of the major parent div
-const objects = document.getElementById('#objectsInSpace');
+const junk = document.getElementById('objectsInSpace');
 
 
-function getReady(){//small little function to help get Rev in position
-  let start = document.querySelector('.button');//
-  start.style.display = 'block';
-  start.style.marginTop = '200px';
-  start.addEventListener('click', evt => revStart());
-  function revStart(){
-    start.style.display = 'none';
+
+function playGame(){//small little function to help get the elements ready
+  // debugger;
+  let start = document.querySelector('.button');//select the start button
+  start.style.display = 'block';//display that sucka
+  start.style.marginTop = '200px';// move it down 200px
+  start.addEventListener('click', evt => getReady());
+  function getReady(){//small little functions to place Rev in place
+    console.log('is this working?')
+    start.style.display = 'none';//
+    revStart();
+    function revStart(){
     player.style.left = "320px";
     player.style.top = "250px";
+    junk.style.left = junk + "2em";
+    scrolling();
+    }
+    function scrolling(){//
+        // debugger;
+        let pos = 0;
+        let force = setInterval(spaceJunk, 1000);//for ever 1000ms, run this function...
+        function spaceJunk(){
+          if (pos >= 700){//if all the objects reach the end of the window...
+            debugger
+            clearInterval(force);//make them stop moving..
+          } else {
+            pos+=100;
+            junk.style.right = pos + "px";
+            console.log("workin");//console.logging to make sure it works
+            }
+        }
+    }
   }
 }
-
-getReady();
-
-// setInterval(spaceJunk, 1000);
-/* function spaceJunk(){
-    objects.style.right = (objects.x * 100).toString() + 'px';
-}
-*/
+playGame();
+// scrolling();
